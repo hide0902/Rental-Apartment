afbc5925e26c4b85337bca40c978955240d085e2
hide0902 <hn.tt.sf.hu@icloud.com>
2020-01-27 17:00:29 +0900

    検索機能実装2

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
-  def self.search(search,search2,search3,search4,search5,search6,search7)
+  def self.search(search,search2,search3,search4,search5,search6,search7,search8,search9,search10,search11,search12,search13,search14,search15,search16,search17,search18,search19,search20,search21)
     return List.all unless search
-    List.where('station = ?', search).where('walk <= ?', search2).where('rent >= ?', search3).where('rent <= ?', search4).where('exclusive >= ?', search5).where('exclusive <= ?', search6).where('age <= ?', search7)
+    List.where('station = ?', search).where('walk <= ?', search2).where('rent >= ?', search3).where('rent <= ?', search4).where('exclusive >= ?', search5).where('exclusive <= ?', search6).where('age <= ?', search7).where('plan = ? or plan = ? or plan = ? or plan = ? or plan = ? or plan = ? or plan = ? or plan = ? or plan = ? or plan = ? or plan = ? or plan = ? or plan = ? or plan = ?', search8, search9, search10, search11, search12, search13, search14, search15, search16, search17, search18, search19, search20, search21)
   end
 end
